fa51572b075bb34938cdc9054522ebca88226805
alvan <alvan.ghon@gmail.com>
2023-03-07 12:38:25 +0700

    first init

---
+++ BlazorWASM/Pages/Counter.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorWASM.Pages
+{
+    public partial class Counter
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+        private void DecrementCount()
+        {
+            if (currentCount != 0) currentCount--;
+        }
+        private void ResetCount()
+        {
+            currentCount = 0;
+        }
+    }
+}
